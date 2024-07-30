747c373f92aae91475010a13084d8b85060f4b94
Jobwafula <jobwafulabg@gmail.com>
2024-07-31 01:27:19 +0300

    our partners

--- hack-repairs/src/components/OurPartners.tsx
+++ hack-repairs/src/components/OurPartners.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Image from 'next/image'
 
 export default function OurPartners() {
   return (
-    <div>
+    <div className='mb-8 ml-8'>
         <h1 className='text-2xl font-bold'>Our Partners</h1>
+        <div >
+          <div className='flex items-center gap-2'><Image src='/mutable.jpeg' width={100}
+            height={100} alt='mutable tech'/>
+            <p className='text-blue-500 text-2xl'>Mutable Tech Entreprises</p></div>  
+            <a href='https://mutabletech.co.ke/' className='hover:text-blue-500 hover:underline'>Click To View  Software Solution available</a>
+        </div>
     </div>
   )
 }
