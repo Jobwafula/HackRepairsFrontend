4b2678b06ade7231fd7a561a335423d3f170178f
Mnangat N <mnangatnathan01@gmail.com>
2024-08-01 21:37:12 +0300

    sell with us and update on extraLinks

--- hack-repairs/src/components/Signin.tsx
+++ hack-repairs/src/components/Signin.tsx
@@ -47,7 +47,7 @@ const Signin = () => {
         </div>
         <button
           type="submit"
-          className="w-full py-2 px-4 bg-blue-500 text-white font-semibold rounded-md shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="w-full py-2 px-4 bg-button  text-white font-semibold rounded-md shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
         >
           Sign In
         </button>
