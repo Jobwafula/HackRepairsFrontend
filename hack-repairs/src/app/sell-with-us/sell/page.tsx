c9767fe092d0a15f8f0563b68a4cf8d48fdd7917
Jobwafula <jobwafulabg@gmail.com>
2024-08-14 17:20:11 +0300

    changes

--- hack-repairs/src/app/sell-with-us/sell/page.tsx
+++ hack-repairs/src/app/sell-with-us/sell/page.tsx
@@ -1,10 +1,14 @@
 import SellWithusForm from "@/components/forms/SellWithusForm";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 import react from "react";
 
 const Sell: React.FC = () => {
   return(
   <div>
+    <Navbar />
     <SellWithusForm />
+    <Footer />
   </div>
 )
   
